57cc06a5bbd20baae00378571dbd7ca867919cf1
Yobuligo <hoffmannpeter82@gmail.com>
2023-02-12 00:22:08 +0100

    Initial commit

---
+++ out/ServiceProviderTypes.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ServiceInstanceType;
+(function (ServiceInstanceType) {
+    ServiceInstanceType[ServiceInstanceType["SINGLE_INSTANTIABLE"] = 0] = "SINGLE_INSTANTIABLE";
+    ServiceInstanceType[ServiceInstanceType["MULTI_INSTANTIABLE"] = 1] = "MULTI_INSTANTIABLE";
+})(ServiceInstanceType = exports.ServiceInstanceType || (exports.ServiceInstanceType = {}));
+//# sourceMappingURL=ServiceProviderTypes.js.map
---
+++ out/serviceProvider/Service.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Service = /** @class */ (function () {
+    function Service() {
+        this.instance = {};
+    }
+    return Service;
+}());
+exports.Service = Service;
+//# sourceMappingURL=Service.js.map
